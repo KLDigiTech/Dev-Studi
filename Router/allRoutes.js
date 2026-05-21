72b7ddf8e8996a3afdcfa94a82b7e3d8a6d2ff80
Kévin Level <kldigitech34@gmail.com>
2026-05-21 09:50:14 +0200

    Page connexion inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,10 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    ];
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
