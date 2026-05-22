2b26109799dc93e7798e4d0afcb05f20259b7855
Kévin Level <kldigitech34@gmail.com>
2026-05-22 10:13:11 +0200

    Formulaire champs requis

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,9 +5,11 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Modifier le mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos Réservations", "/pages/reservations/allResa.html"),
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
